4aaf966b0af58b74e128e86f718087a153b4662e
Smruti Ranjan <ranjanbehera26@gmail.com>
2026-06-04 00:04:55 +0530

    build-bug-issue-7

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   },
   nitro: {
     preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+      serverDir: ".vercel/output/functions/__server.func",
+      publicDir: ".vercel/output/static",
+    },
   },
 });
